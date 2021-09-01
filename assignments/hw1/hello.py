f6e7e15e61507c6b3a9d00a20ff3eb5f1ffc9cec
NeideighJ <neideighj@g.cofc.edu>
2021-09-01 14:44:35 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,11 @@
+"""
+Neideigh, Jonathan
+hello.py
+
+Problem: Write a program that prints “hello, world!” to the screen.
+
+Certification of Authenticity: I certify that this assignment is entirely my work.
+"""
+
+def main():
+    print("hello, world!")
